ced87e711c9457c05b152caf3e9f63930dca8dfd
hyunyoonsu <yshyun777@gmail.com>
2026-05-26 15:02:40 +0900

    update_App

---
+++ .next/server/app/api/collections/route.js
@@ -0,0 +1,7 @@
+var R=require("../../../chunks/[turbopack]_runtime.js")("server/app/api/collections/route.js")
+R.c("server/chunks/[root-of-the-server]__07_aczv._.js")
+R.c("server/chunks/node_modules_@supabase_supabase-js_dist_index_mjs_0hp37pu._.js")
+R.c("server/chunks/[root-of-the-server]__0j8-xkl._.js")
+R.c("server/chunks/_next-internal_server_app_api_collections_route_actions_0.oxdfu.js")
+R.m(78690)
+module.exports=R.m(78690).exports
---
+++ .next/server/chunks/_next-internal_server_app_api_cluster_route_actions_0th59jv.js
@@ -0,0 +1,3 @@
+module.exports=[45424,(e,o,d)=>{}];
+
+//# sourceMappingURL=_next-internal_server_app_api_cluster_route_actions_0th59jv.js.map
