4906912ecf572b2db66dd575e81d1e9cabacb175
SeoYunyyy <comari03@naver.com>
2026-05-27 10:19:23 +0900

    변수이름수정

--- hyoju_connection.py
+++ hyoju_connection.py
@@ -19,7 +19,7 @@ hyoju_bp = Blueprint('hyoju_connection', __name__)
 
 supabase = create_client(
     os.getenv("SUPABASE_URL"),
-    os.getenv("SUPABASE_SERVICE_ROLE_KEY"),
+    os.getenv("SUPABASE_SERVICE_KEY"),
 )
 openai_client = OpenAI()
 
